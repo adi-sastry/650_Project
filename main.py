dad4b969443619c3d69313b8bb807f0efdbcba06
peter18775 <prakhartiwari876@gmail.com>
2025-10-31 01:18:11 -0400

    create config and restructure

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import provision_resources as pr
-import clean_up as cu
-import simulate_image_streaming
-import config
+import utils.provision_resources as pr
+import utils.clean_up as cu
+import src.simulate_image_streaming as simulate_image_streaming
 import os
 import sys
+import yaml
 
+def load_config(config_path: str= 'config.yaml') -> dict:
+  with open(config_path, 'r') as f:
+    config_data = yaml.safe_load(f)
+  return config_data
+
+def delete_resources(cfg:dict, iam_client, ct_policy_arn):
+  print("Begining Clean-Up of AWS Reaources...\n")
+
+ #Delete from-camera-trap s3 bucket and IAM policy (Comment out if we want to retain resources)
+  cu.delete_all_objects_in_s3(cfg['CAMERA_TRAP']['bucket_name'],cfg['CAMERA_TRAP']['region'])
+  cu.delete_s3_bucket(cfg['CAMERA_TRAP']['bucket_name'],cfg['CAMERA_TRAP']['region'])
+  cu.delete_iam_policy(iam_client,ct_policy_arn, cfg['CAMERA_TRAP']['user_name'])
+  print(f"Bucket {cfg['CAMERA_TRAP']['bucket_name']} and policy {ct_policy_arn} deleted successfully.\n")
 
 def main():
+ 
+ #----------------------------Load Configuration----------------------------#
+ cfg = load_config('config.yaml')
+
  #----------------------------Verifying dataset was downloaded from Kaggle---------------------------#
 
  marker_path = os.path.join("data", ".download_complete")
@@ -20,26 +37,30 @@ def main():
  print("Begining creation of AWS Reaources...\n")
 
  #Provisioning from-camera-trap S3 Bucket and Policy
- pr.create_s3_bucket(config.CAMERA_TRAP['bucket_name'], config.CAMERA_TRAP['region'])
- ct_policy_arn = pr.create_image_camera_trap_policy_for_bucket(config.CAMERA_TRAP['bucket_name'], config.CAMERA_TRAP['user_name'],config.CAMERA_TRAP['allow_delete'])
+ pr.create_s3_bucket(cfg['CAMERA_TRAP']['bucket_name'], 
+                     cfg['CAMERA_TRAP']['region'])
+ 
+ ct_policy_arn = pr.create_image_camera_trap_policy_for_bucket(
+   cfg['CAMERA_TRAP']['bucket_name'], 
+   cfg['CAMERA_TRAP']['user_name'],
+   cfg['CAMERA_TRAP']['allow_delete'])
+ 
  iam_client = pr.get_iam_client()
- pr.attach_policy_user(iam_client,ct_policy_arn, config.CAMERA_TRAP['user_name'])
- print(f"Bucket {config.CAMERA_TRAP['bucket_name']} and policy {ct_policy_arn} created successfully.\n")
+
+ pr.attach_policy_user(
+   iam_client,ct_policy_arn, 
+   cfg['CAMERA_TRAP']['user_name'])
+ 
+ print(f"Bucket {cfg['CAMERA_TRAP']['bucket_name']} and policy {ct_policy_arn} created successfully.\n")
 
 #----------------------------Begin Simulation----------------------------#
 
  #Simulation of streaming camera trap data directly into from-camera-trap s3 Bucket. These are images from validation set from data folder.
  print("Begining simulation of images from camera trap...\n")
- simulate_image_streaming.simulation(config.CAMERA_TRAP['root_dir'], config.CAMERA_TRAP['bucket_name'], config.CAMERA_TRAP['val_meta'])
+ simulate_image_streaming.simulation(cfg['CAMERA_TRAP']['root_dir'], cfg['CAMERA_TRAP']['bucket_name'], cfg['CAMERA_TRAP']['val_meta'])
 
  #----------------------------Pipeline complete. Delete resources to avoid charges----------------------------#
- print("Begining Clean-Up of AWS Reaources...\n")
-
- #Delete from-camera-trap s3 bucket and IAM policy (Comment out if we want to retain resources)
- cu.delete_all_objects_in_s3(config.CAMERA_TRAP['bucket_name'],config.CAMERA_TRAP['region'])
- cu.delete_s3_bucket(config.CAMERA_TRAP['bucket_name'],config.CAMERA_TRAP['region'])
- cu.delete_iam_policy(iam_client,ct_policy_arn, config.CAMERA_TRAP['user_name'])
- print(f"Bucket {config.CAMERA_TRAP['bucket_name']} and policy {ct_policy_arn} deleted successfully.\n")
+ delete_resources(cfg, iam_client, ct_policy_arn)
 
 if __name__ =="__main__":
  main()
